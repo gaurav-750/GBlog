1abc63898f1c76a14db514441a78d0df20e07acf
Gaurav <gauravsomani52750@gmail.com>
2022-07-09 12:12:11 +0530

    custom hook

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,50 +1,14 @@
-import { useState, useEffect } from "react";
-import {Blogs} from '../blogs';
+import useFetch from '../useFetch';
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState('');
-
-    const title = "All Blogs!";
+    const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     const handleDelete = (id) => {
-        setBlogs(blogs.filter((blog) => {
-            return (
-                blog.id != id
-            )
-        }))
+        console.log('id in delete', id);
     }
 
-    //We can use useEffect to fire a function everytime the component renders itself!
-    useEffect(() => { //This function is going to load everytime the component renders
-        console.log('useEffect!');
-
-        fetch('http://localhost:8000/blogs')
-            .then((res) => {
-                if (!res.ok) { //if the req.cannot be fetched, throw an error => it'll go in catch block
-                    throw Error("Oops! Could not fetch the request!");
-                }
-
-                return res.json();
-            })
-            .then((data) => {
-                // console.log('data', data);
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch((err) => {
-                setError(err.message);
-                setIsPending(false);
-            })
-
-
-    }, []); 
-    //Whatever you put in the dependency array, if that changes, useEffect will take place
-    //i.e it'll invoke the function inside it
 
     return ( 
         <div className="home">
@@ -55,7 +19,7 @@ const Home = () => {
             {blogs &&
                 <BlogList 
                 blogs = {blogs}
-                title = {title}
+                title = "All Blogs!"
                 handleDelete = {handleDelete}
             />
             }
---
+++ src/useFetch.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState('');
+
+
+    //We can use useEffect to fire a function everytime the component renders itself!
+    useEffect(() => { //This function is going to load everytime the component renders
+        console.log('useEffect!');
+
+        fetch(url)
+            .then((res) => {
+                if (!res.ok) { //if the req.cannot be fetched, throw an error => it'll go in catch block
+                    throw Error("Oops! Could not fetch the request!");
+                }
+
+                return res.json();
+            })
+            .then((data) => {
+                // console.log('data', data);
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setIsPending(false);
+            })
+
+
+    }, [url]);
+    
+    return {data, isPending, error};
+}
+//Whatever you put in the dependency array, if that changes, useEffect will take place
+//i.e it'll invoke the function inside it
+
+export default useFetch;
